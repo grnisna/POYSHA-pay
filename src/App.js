78df93639f23f0ffadd78c58f747b749857cac18
grnisan <mdnisanahmed63@gmail.com>
2022-07-19 11:05:29 +0600

    demo code and images are writter if you need you can change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div>
-      <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1>      
+      <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1>  
+      <h2> Project Type: We will create an f-commerce website like bkash, upay,Paypal ect.</h2>    
     </div>
   );
 }
