8d60d02c478d352aa13d1d0d6234e5c6025c2b4f
projit-1 <projitmondol@gmail.com>
2022-07-22 02:26:58 +0600

    trusted company is ready to added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import CardSlide from "./components/Pages/HomePage/CardSlide";
+
+
 function App() {
   return (
     <div>
-      <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1>      
+      <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1>
+      <CardSlide></CardSlide>
     </div>
   );
 }
