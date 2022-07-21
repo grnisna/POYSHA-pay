cbf8a4b73584c3c5d415bf3e313e2bb2a3f4a876
MEHWDI HASAN MUGDHO <mehedimugdho72@gmail.com>
2022-07-22 03:44:32 +0600

    add growth section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Home from "./components/Pages/HomePage/Home";
+
 function App() {
   return (
     <div>
-      <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1>      
+      <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1> 
+      <Home></Home>
     </div>
   );
 }
--- src/components/Pages/HomePage/Home.js
+++ src/components/Pages/HomePage/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Growth from './Growth';
 
 const Home = () => {
     return (
         <div>
-            
+            <Growth></Growth>
         </div>
     );
 };
