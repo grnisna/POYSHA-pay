f72b2b6069a771ce90680d26acb8327fd60ee910
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-07-24 00:21:31 +0600

    Image Change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 import AppDownload from "./components/Pages/HomePage/AppDownload";
-=======
-import Footer from "./components/SharedCompo/Footer/Footer";
-import Navbar from "./components/SharedCompo/Navbar/Navbar";
-
->>>>>>> c229b26b22d07858851974eba32281b4819bf383
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <AppDownload></AppDownload>
-=======
-      <Navbar />
-      <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1>
-      <Footer />
->>>>>>> c229b26b22d07858851974eba32281b4819bf383
     </div>
   );
 }
