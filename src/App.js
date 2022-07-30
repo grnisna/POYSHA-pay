2f28ad90376b697ea57bc213340a0768edc2a8b3
projit-1 <projitmondol@gmail.com>
2022-07-31 00:00:46 +0600

    dash nav is added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-<<<<<<< HEAD
+import DashboardNav from "./components/Dashboard/Shared/DashboardNav";
+import SendMoney from "./components/Dashboard/Shared/SendMoney";
 import CardSlide from "./components/Pages/HomePage/CardSlide";
 
-=======
 import Home from "./components/Pages/HomePage/Home";
+import Users from "./components/Pages/HomePage/Users";
 import Footer from "./components/SharedCompo/Footer/Footer";
 import Navbar from "./components/SharedCompo/Navbar/Navbar";
->>>>>>> a026eba897b883700cf84a5e694f6d0d7356aee4
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <h1 className="text-center">The POYSHA-pay is F-Commerce Wallet</h1>
       <CardSlide></CardSlide>
-=======
       <Navbar />
       <Home></Home>
+      <Users></Users>
+      <DashboardNav></DashboardNav>
+      <SendMoney></SendMoney>
       <Footer />
->>>>>>> a026eba897b883700cf84a5e694f6d0d7356aee4
     </div>
   );
 }
