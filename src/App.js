2ab5e529e6837b1720cf0ea714362b8cff61125b
MEHWDI HASAN MUGDHO <mehedimugdho72@gmail.com>
2022-08-09 00:19:53 +0600

    add messenger

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
+<<<<<<< HEAD
 // import AppDownload from "./components/Pages/HomePage/AppDownload";
+=======
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login/Login";
+import SignUp from "./components/Auth/SignUp/SignUp";
+import RequireAuth from "./components/Auth/RequireAuth/RequireAuth";
+>>>>>>> e80776bfe95c041d7afca0638e21cb4fe0d1a0be
 import Home from "./components/Pages/HomePage/Home";
+import AddMoney from "./components/Services/AddMoney";
 import Footer from "./components/SharedCompo/Footer/Footer";
 import Navbar from "./components/SharedCompo/Navbar/Navbar";
+<<<<<<< HEAD
+=======
+import { ToastContainer } from 'react-toastify';
+import SendMoney from "./components/Dashboard/Shared/SendMoney";
+import OthersAccounts from "./components/Dashboard/OthersAccounts/OthersAccounts";
+import Dashboard from "./components/Dashboard/MyAccount/Dashboard";
+import RechargeMoney from "./components/Services/RechargeMoney";
+// import SendMoney2 from "./components/Dashboard/Shared/SendMoney2";
+
+>>>>>>> e80776bfe95c041d7afca0638e21cb4fe0d1a0be
 
 function App() {
   return (
     <div>
+<<<<<<< HEAD
       {/* <AppDownload></AppDownload> */}
       <Navbar />
       <Home></Home>
       <Footer />
+=======
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/addMoney' element={<RequireAuth><AddMoney /> </RequireAuth>}></Route>
+        <Route path='/sendMoney' element={<RequireAuth><SendMoney /></RequireAuth>}></Route>
+        <Route path='/dashboard' element={<RequireAuth> <Dashboard></Dashboard></RequireAuth>}></Route>
+        <Route path='/addAccount' element={<RequireAuth> <OthersAccounts></OthersAccounts> </RequireAuth>}></Route>
+        <Route path='/rechargeMoney' element={<RequireAuth> <RechargeMoney></RechargeMoney></RequireAuth>}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
+      </Routes>
+      <Footer />
+      <ToastContainer />
+
+>>>>>>> e80776bfe95c041d7afca0638e21cb4fe0d1a0be
     </div>
   );
 }
--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import BGLogin from '../../../Assets/bg-login2.jpg';
+import { useForm } from "react-hook-form";
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import axios from 'axios';
+import LoginImage from '../../../Assets/Login/Login.png';
+
+
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location?.state?.from?.pathname || '/dashboard';
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error
+    ] = useSignInWithEmailAndPassword(auth);
+
+
+    const onSubmit = async event => {
+        await signInWithEmailAndPassword(event.email, event.password);
+    };
+
+    useEffect(() => {
+        if (user) {
+            async function getToken() {
+                const email = user.email;
+                const { data } = await axios.post('https://powerful-basin-90376.herokuapp.com/login', { email });
+                localStorage.setItem('AccessToken', data);
+                navigate(from, { replace: true });
+            }
+
+            getToken();
+        }
+    }, [navigate, user, from])
+
+
+
     return (
-        <div>
-            
-        </div>
+        <div className="flex flex-col items-center justify-center h-screen p-5"
+            style={{ backgroundImage: `url(${BGLogin})`, backgroundSize: 'cover' }}>
+            <div className='lg:flex lg:justify-between lg:w-[1000px] lg:shadow-xl  lg:bg-clip-padding lg:backdrop-filter lg:opacity-90 lg:backdrop-blur-md lg:rounded-md'>
+                <div className='lg:w-96  md:w-96 sm:w-96 flex items-center '>
+                    <img src={LoginImage} alt="" />
+                </div>
+
+                <div className='lg:w-96 md:w-96 sm:w-96 shadow-xl bg-clip-padding backdrop-filter bg-white bg-opacity-50 backdrop-blur-md py-10 px-8 rounded-md'>
+                    <h1 className="mb-10 text-3xl font-bold text-center text-black">Login</h1>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                            <input type="email" className='input w-full max-w-md rounded-3xl mt-1 mb-3 '{...register("email")} placeholder="Type Your Email" autocomplete="off" required />
+                        </div>
+
+                        <input type="password" className='input w-full max-w-md mt-1 h-10 rounded-3xl'{...register("password")} placeholder="Type Your Password" autocomplete="off" required />
+
+                        <input className='mt-7 bg-white bg-opacity-0 hover:bg-opacity-90 hover:rounded-3xl transition duration-500 rounded-md shadow-sm p-3 w-full font-semibold cursor-pointer' type="submit" value="LOGIN" />
+                    </form>
+                </div>
+            </div>
+        </div >
     );
 };
 
