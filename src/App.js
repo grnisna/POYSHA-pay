9df3f092fb50a8648c70bb0b83a7b1d19719d768
ShimulChandraDas <msshimul99@gmail.com>
2022-08-20 23:00:55 +0600

    added reachareg change

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 
-
-// import AppDownload from "./components/Pages/HomePage/AppDownload";
-
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login/Login";
 import RequireAuth from "./components/Auth/RequireAuth/RequireAuth";
-
 import SignUp from "./components/Auth/SignUp/SignUp";
-
-
-
 import Home from "./components/Pages/HomePage/Home";
 import AddMoney from "./components/Services/AddMoney";
 import Footer from "./components/SharedCompo/Footer/Footer";
 import Navbar from "./components/SharedCompo/Navbar/Navbar";
-
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Account from "./components/Dashboard/MyAccount/Account";
-
 
 import SendMoney from "./components/Dashboard/Shared/SendMoney";
 import OthersAccounts from "./components/Dashboard/OthersAccounts/OthersAccounts";
 import Dashboard from "./components/Dashboard/MyAccount/Dashboard";
-import RechargeMoney from "./components/Services/RechargeMoney";
-import SubFooter from './components/SharedCompo/Footer/SubFooter';
 import ViewAllTransaction from "./components/Dashboard/Shared/ViewAllTransaction/ViewAllTransaction";
 // import SendMoney2 from "./components/Dashboard/Shared/SendMoney2";
-import DashboardNav from "./components/Dashboard/DashboardNav/DashboardNav";
+
 import TakeLone from "./components/Dashboard/TakeLone/TakeLone";
 import DashboardOverView from "./components/Dashboard/MyAccount/DashboardOverView";
 import AddReview from "./components/Dashboard/AddReview/AddReview";
--- src/components/SharedCompo/Navbar/Navbar.js
+++ src/components/SharedCompo/Navbar/Navbar.js
@@ -24,7 +24,8 @@ const Navbar = () => {
         <li><NavLink to='/sendMoney' >Send Money</NavLink></li>
         <li><NavLink to='/addMoney' >Add Money</NavLink></li>
         <li><NavLink to='/addAccount' >Add Account</NavLink></li>
-        <li><a>Recharge</a></li>
+        <li><NavLink to='/addAccount' >Add Account</NavLink></li>
+        <li><NavLink to='/recharge'>Recharge</NavLink></li>
       </ul>
     </li>
   </>
@@ -48,7 +49,7 @@ const Navbar = () => {
             <label tabIndex="0" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2   bg-white w-32 border">
+            <ul tabIndex="0" className="menu menu-compact text-secondary dropdown-content mt-3 p-2   bg-white w-32 border">
               {menu}
 
               <li><NavLink to='/dashboard'>My poisa</NavLink></li>
