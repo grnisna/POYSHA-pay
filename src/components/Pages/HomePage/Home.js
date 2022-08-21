f062e115a7dfc43f3a41238c61d2d70553414ac8
projit-1 <projitmondol@gmail.com>
2022-08-22 00:02:29 +0600

    FAQs

--- src/components/Pages/HomePage/Home.js
+++ src/components/Pages/HomePage/Home.js
@@ -3,10 +3,10 @@ import AppDownload from './AppDownload';
 import Services from './Services/Services';
 import HeroBanner from './HeroBanner/HeroBanner';
 import CardSlide from './CardSlide';
-
 import Users from './Users';
-
 import Review from './Reviews/Review';
+import Questions from './Questions';
+
 
 
 
@@ -14,7 +14,7 @@ const Home = () => {
     return (
         <div>
             <HeroBanner></HeroBanner>
-
+            {/* <Questions></Questions> */}
             <CardSlide></CardSlide>
             {/* <Users></Users> */}
             <Services></Services>
