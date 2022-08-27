de9295278ddcbe2ebdc37497865ee974498a7632
grnisan <mdnisanahmed63@gmail.com>
2022-08-28 02:10:46 +0600

    setting finished 30 percent

--- src/components/Dashboard/MyAccount/Setting.js
+++ src/components/Dashboard/MyAccount/Setting.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ImageUpload from '../../Pages/HomePage/ImageUpload';
 import UserImages from '../../Pages/HomePage/UserImages';
+import SettingDemo from './SettingDemo';
 
 const Setting = () => {
     return (
         <div>
             <ImageUpload></ImageUpload>
             <UserImages></UserImages>
+            <SettingDemo/>
         </div>
     );
 };
